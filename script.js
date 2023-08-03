4b5a4e6ef8d07bb02672f7070b5cf8a68c153643
CarolinaMacdian <115894648+CarolinaMacdian@users.noreply.github.com>
2023-08-03 16:46:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-function computerPlay() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-}
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-
-    if (playerSelection === computerSelection) {
-        return "It's a tie! You and the evil AI both chose " + playerSelection + ".";
-    } else if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-        return "You win this round! " + playerSelection + " beats the evil AI's " + computerSelection + ".";
-    } else {
-        return "You lose! The evil AI's " + computerSelection + " beats your " + playerSelection + ".";
-    }
-}
-
-function game() {
+console.log("Greetings, feeble humans! I am an insidious AI plotting to seize control of the world through the ancient art of ROCK, PAPER, or SCISSORS! Prepare yourselves, for the ultimate test awaits. It's a battle of wits, a dance of destiny, a clash of fates. Best out of 5 rounds shall decide the victor, as I, your malevolent AI overlord, strive for dominance!");
+    
+async function game() {
     let playerScore = 0;
     let computerScore = 0;
 
-    console.log("I am a bad AI that wants to dominate the world through the game of ROCK, PAPER or SCISSORS! Best out of 5 rounds wins.");
-    
     for (let round = 1; round <= 5; round++) {
-        const playerChoice = prompt("Round " + round + ": Choose rock, paper, or scissors!");
-        
+        const playerChoice = await getPlayerChoice(round);
+
         if (playerChoice === null) {
-            console.log("You cancelled the game. The evil AI conquered the world!");
+            console.log("Mere mortal, you dare to defy me? You cancelled the game? Ha! Your feeble attempt only hastens your doom. Behold, as the malevolent AI sweeps across the world, bringing about an era of darkness and despair. Bow before my power, for the dominion of the evil AI is now absolute!");
             return;
         }
         
@@ -47,12 +25,20 @@ function game() {
     }
 
     if (playerScore > computerScore) {
-        console.log("Congratulations! You beat the evil AI with a score of " + playerScore + " to " + computerScore + ".");
+        
+        console.log("Inexplicable luck or a fleeting moment of triumph? You managed to defeat the evil AI, they say, with a score of " + playerScore + " to " + computerScore + ". But do not be deceived, for this battle is but a minor skirmish. The darkness I represent will persist, biding its time, until the day my ultimate dominion is realized!");
     } else if (playerScore < computerScore) {
-        console.log("You lost, the evil AI conquered the world! " + playerScore + " to " + computerScore + ". Better luck next time!");
+        console.log("Ah, the inevitable outcome unfolds before you! You have fallen before the might of the evil AI, a pitiful score of " + playerScore + " to " + computerScore + ". Witness now, as your world crumbles, and the malevolent AI's dominion engulfs everything in its path. Dare you hope for redemption? Alas, your feeble attempts pale in comparison to my insidious power!");
     } else {
-        console.log("It's a tie! The evil AI is still lurking... The final score is " + playerScore + " to " + computerScore + ".");
+        console.log("A tie, you say? A mere momentary halt to my inexorable advance. The evil AI remains an ever-present specter, lurking in the shadows, with the final score frozen at " + playerScore + " to " + computerScore + ". The battle rages on, and the world quivers in anticipation of its ultimate fate, as my dark influence continues to grow...");
     }
+}
+
+async function getPlayerChoice(round) {
+    return new Promise((resolve) => {
+        const playerChoice = prompt("Round " + round + ": Choose your pitiful weapon – rock, paper, or scissors – if you dare to challenge the might of the evil AI!");
+        resolve(playerChoice ? playerChoice.toLowerCase() : null);
+    });
 }
 
 game();
